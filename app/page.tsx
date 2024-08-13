c58ebed9ca07151ea1b6955ac6114666e9b802cf
Fahreza Pasha Haikal <abufahreza@gmail.com>
2024-08-14 05:20:25 +0700

    update navbar

--- app/page.tsx
+++ app/page.tsx
@@ -5,34 +5,46 @@ import { VelocityScroll } from "@/components/magicui/scroll-based-velocity";
 export default function Home() {
   return (
     <>
-      <div className="relative flex flex-col h-screen">
+      <div id="home" className="relative flex flex-col h-screen">
+        {/* VelocityScroll berada di layer paling bawah */}
+        <div className="absolute inset-0 flex justify-center items-center">
+          <VelocityScroll
+            text="FPHaikal "
+            default_velocity={1}
+            className="font-display text-center text-4xl font-bold tracking-[-0.02em] text-primary-500 drop-shadow-sm md:text-7xl md:leading-[5rem]"
+          />
+        </div>
+        {/* BlurIn berada di atas VelocityScroll */}
+        <div className="absolute inset-0 flex justify-center items-center z-10">
+          <Image
+            alt="Mountains"
+            src={`/te.png`}
+            quality={100}
+            fill
+            sizes="100vw"
+            style={{
+              objectFit: 'cover',
+            }}
+          />
+        </div>
+      </div>
+      <div id="services" className="relative flex flex-col h-screen">
         {/* VelocityScroll berada di layer paling bawah */}
         <div className="absolute inset-0 flex justify-center items-center">
           <VelocityScroll
             text="Hello World👋"
-            default_velocity={5}
+            default_velocity={1}
             className="font-display text-center text-4xl font-bold tracking-[-0.02em] text-primary- drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]"
           />
         </div>
         {/* BlurIn berada di atas VelocityScroll */}
         <div className="absolute inset-0 flex justify-center items-center z-10">
           <BlurIn word="Hello World" className="text-4xl font-bold text-white md:text-7xl md:leading-[5rem]" />
-        </div>
-      </div>
-      <div className="relative flex flex-col mt-28 h-screen">
-        {/* VelocityScroll berada di layer paling bawah */}
-        <div className="absolute inset-0 flex justify-center items-center">
-          <VelocityScroll
-            text="Hello World👋"
-            default_velocity={5}
-            className="font-display text-center text-4xl font-bold tracking-[-0.02em] text-primary- drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]"
-          />
-        </div>
-        {/* BlurIn berada di atas VelocityScroll */}
-        <div className="absolute inset-0 flex justify-center items-center z-10">
+          <BlurIn word="Hello World" className="text-4xl font-bold text-white md:text-7xl md:leading-[5rem]" />
           <BlurIn word="Hello World" className="text-4xl font-bold text-white md:text-7xl md:leading-[5rem]" />
         </div>
       </div>
+
     </>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,18 @@
+"use client";
+
 import { HomeIcon } from "./ui/icons"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export default function Navbar() {
   return(
     <>
       <div className="fixed top-0 z-[999] flex w-full justify-center mt-10">
-        <div className="flex p-3 bg-gradient-to-br from-dark-2 to-dark border-dark border shadow-lg justify-center items-center rounded-full">
-          <Link href="#" className="flex px-5 py-3 bg-primary-500 rounded-full items-center gap-1">
+        <div className="flex p-3 gap-1 bg-gradient-to-br from-dark-2 to-dark border-dark border shadow-lg justify-center items-center rounded-full">
+          <Link href="#home" className="flex px-5 py-3 bg-dark/50 hover:bg-primary-500 transition-all duration-500 rounded-full items-center gap-1">
             <h1 className="text-white">Home</h1>
           </Link>
-          <Link href="#" className="flex px-5 py-3 bg-primary-500 rounded-full items-center gap-1">
+          <Link href="#services" className="flex px-5 py-3 bg-dark/50 hover:bg-primary-500 transition-all duration-500 rounded-full items-center gap-1" >
             <h1 className="text-white">Services</h1>
           </Link>
         </div>
