bc6439b84f2a9f86e374db0e93adb76091eeefa4
Fahreza Pasha Haikal <abufahreza@gmail.com>
2024-10-11 06:24:55 +0700

    update information fph

--- app/FPHaikal/page.tsx
+++ app/FPHaikal/page.tsx
@@ -9,10 +9,10 @@ import { ElectionIcon, EmailIcon, InstagramIcon, JapanIcon, LinkedinIcon, Locati
 import { VelocityScroll } from "@/components/magicui/scroll-based-velocity"
 import ShimmerButton from "@/components/magicui/shimmer-button"
 import CopyButton from "@/components/copy-button"
-import { PracticumExperience } from "@/components/naira-section/practicum-experience";
-import AboutMe from "@/components/naira-section/about-me";
+import { PracticumExperience } from "@/components/fph-section/practicum-experience";
+import AboutMe from "@/components/fph-section/about-me";
 
-export default function NairaSalima() {
+export default function FPHaikal() {
   const [iconSize, setIconSize] = useState("30");
 
   useEffect(() => {
@@ -45,14 +45,14 @@ export default function NairaSalima() {
         {/* VelocityScroll berada di layer paling bawah */}
         <div className="absolute inset-0 flex justify-center items-center">
           <VelocityScroll
-            text="Naira Salima "
+            text="Fahreza Haikal "
             default_velocity={1}
             className="font-display text-center text-4xl font-bold tracking-[-0.02em] bg-gradient-to-br from-primary-500 to-dark-2 text-transparent bg-clip-text md:text-7xl md:leading-[5rem]"
           />
         </div>
         {/* BlurIn berada di atas VelocityScroll */}
         <div className="absolute inset-0 flex items-center z-10">
-          <Image alt="Me" src={`/naira.png`} quality={100} fill loading="lazy" className=""
+          <Image alt="Me" src={`/fph.png`} quality={100} fill loading="lazy" className=""
             style={{
               objectFit: 'cover',
             }}
@@ -60,32 +60,32 @@ export default function NairaSalima() {
           <div className="w-1/2 flex justify-center ms-0 xl:ms-52 mt-56 md:mt-0 lg:mt-72 xl:mt-32">
             <div className="w-fit">
               <div className="relative z-30 rounded-2xl m-4 p-8 md:p-14 bg-gradient-to-br from-dark-2 to-dark border-dark border-2 shadow-lg">
-                <div className="flex flex-col gap-1">
+              <div className="flex flex-col gap-1">
                   <h1 className="text-primary-50 font-normal md:text-4xl">Hi,👋</h1>
-                  <h1 className="text-primary-500 font-bold md:text-4xl"><span className="text-primary-50">I&apos;m</span> Naira Salima</h1>
+                  <h1 className="text-primary-500 font-bold md:text-4xl"><span className="text-primary-50">I&apos;m</span> Fahreza Haikal</h1>
                   <div className="flex items-center justify-between">
-                    <div className="hidden xl:flex w-16 outline outline-1 outline-white" />
-                    <h1 className="text-primary-50 font-bold text-xs md:text-xl text-start md:text-end font-secondary w-fit">Analytical Chemistry</h1>
+                    <div className="hidden xl:flex w-32 outline outline-1 outline-white" />
+                    <h1 className="text-white font-bold text-xs md:text-lg text-start md:text-end font-secondary w-fit">Fullstack Developer</h1>
                   </div>
                   <div className="z-10 flex flex-col xl:flex-row items-start xl:items-center mt-4 gap-2">
                     <ShimmerButton shimmerColor="#f0fdf1" background="linear-gradient(90deg, rgba(22,81,30,1) 0%, rgba(39,192,56,1) 100%)" className="shadow-2xl">
-                      <span className="whitespace-pre-wrap text-center text-xs font-medium leading-none tracking-tight text-primary-50 dark:from-white dark:to-slate-900/10 lg:text-lg">
+                      <span className="whitespace-pre-wrap text-center text-xs font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
                         Hire Me!
                       </span>
                     </ShimmerButton>
-                    <button className="px-4 py-2.5 bg-primary-50/10 text-primary-50 rounded-full hover:bg-primary-50 hover:text-primary-500 transition-all duration-200 whitespace-pre-wrap text-center text-xs font-medium leading-none tracking-tight lg:text-lg">
+                    <Link href={'FPHaikal'} className="px-4 py-2.5 bg-primary-50/10 text-primary-50 rounded-full hover:bg-primary-50 hover:text-primary-500 transition-all duration-200 whitespace-pre-wrap text-center text-xs font-medium leading-none tracking-tight lg:text-lg">
                       Learn More
-                    </button>
-                  </div>
+                    </Link>
+                  </div> 
                   <h1 className="text-primary-50 font-normal text-xs md:text-lg mt-3 md:mt-6">Follow Me</h1>
                   <div className="flex gap-1 md:gap-3 mt-1 md:mt-2">
-                    <Link href="https://instagram.com/nayzho_ken" target="_blank" rel="noopener noreferrer">
+                    <Link href="https://instagram.com/fp_haikal" target="_blank" rel="noopener noreferrer">
                       <InstagramIcon color="white" width={iconSize} />
                     </Link>
-                    <Link href="https://linkedin.com/in/nairasalima" target="_blank" rel="noopener noreferrer">
+                    <Link href="https://linkedin.com/in/fphaikal" target="_blank" rel="noopener noreferrer">
                       <LinkedinIcon color="white" width={iconSize} />
                     </Link>
-                    <CopyButton text="nairasalima07@gmail.com" color="white" width={iconSize} />
+                    <CopyButton text="abufahreza@gmail.com" color="white" width={iconSize} />
                   </div>
                 </div>
               </div>
---
+++ components/fph-section/about-me.tsx
@@ -0,0 +1,64 @@
+import Image from "next/image"
+import { ElectionIcon, EmailIcon, InstagramIcon, JapanIcon, LinkedinIcon, LocationIcon, OrganizationIcon, PhoneIcon } from "@/components/ui/icons"
+import RetroGrid from "@/components/magicui/retro-grid";
+
+
+export default function AboutMe() {
+  return (
+    <div id="about" className="relative flex w-full min-h-screen items-center justify-center overflow-hidden rounded-lg md:shadow-xl">
+      <div className="flex flex-col 2xl:flex-row h-full w-full gap-5 2xl:gap-10 px-5 py-3 2xl:px-80 2xl:py-32">
+        <div className="flex flex-col w-full 2xl:w-1/2 gap-5 2xl:gap-10 rounded-xl">
+          <div className="h-fit 2xl:h-2/3 bg-gradient-to-br from-dark-2 to-dark border-dark border-2 shadow-lg rounded-xl p-10 2xl:p-20 hover:-translate-y-4 hover:scale-105 duration-700">
+            <div className="flex flex-col gap-10">
+              <div className="flex gap-10 items-center">
+                <div className="flex h-40 w-40 items-center">
+                  <Image className="h-fit w-fit rounded-3xl object-cover object-center " src={"/profile.png"} alt="Profile" width={500} height={500} loading="lazy" />
+                </div>
+                <div className="flex flex-col justify-center">
+                  <h1 className="text-md 2xl:text-xl text-primary-50">Let Me Introduce Myself</h1>
+                  <h1 className="text-primary-50 font-bold text-4xl 2xl:text-5xl">About <span className="text-primary-500">Me</span></h1>
+                </div>
+              </div>
+              <div className="bg-dark p-6 rounded-xl">
+                <h1 className="text-primary-50 text-md 2xl:text-lg">Hello. My name is Fahreza Pasha Haikal and please call me Haikal. I come from Gresik and I&apos;m currently in my second year studying Mechatronic Engineering at SMK-SMTI Yogyakarta (Yogyakarta Vocational High School of Industrial Technology). I was born in Gresik on the 16th December 2006</h1>
+              </div>
+            </div>
+          </div>
+          <div className="h-fit 2xl:h-1/3 grid grid-cols-2 gap-5 2xl:gap-10">
+            <div className="flex flex-col gap-1 bg-gradient-to-br from-dark-2 to-dark border-dark border-2 shadow-lg rounded-xl p-5 2xl:p-10 justify-end hover:-translate-y-4 hover:scale-105 duration-700">
+              <PhoneIcon color="primary-500" width="50" />
+              <h1 className="text-primary-50 text-lg 2xl:text-2xl font-bold">+62 857-6590-9380</h1>
+              <h1 className="text-primary-50 text-md 2xl:text-xl">Phone</h1>
+            </div>
+            <div className="flex flex-col gap-1 bg-gradient-to-br from-dark-2 to-dark border-dark border-2 shadow-lg rounded-xl p-5 2xl:p-10 justify-end hover:-translate-y-4 hover:scale-105 duration-700">
+              <LocationIcon color="primary-500" width="50" />
+              <h1 className="text-primary-50 text-lg 2xl:text-2xl font-bold">Yogyakarta, Indonesia</h1>
+              <h1 className="text-primary-50 text-md 2xl:text-xl">Location</h1>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col w-full 2xl:w-1/3 gap-5 2xl:gap-10">
+          <div className="flex flex-col gap-2 h-1/3 bg-gradient-to-br from-dark-2 to-dark border-dark border-2 shadow-lg rounded-xl p-10 hover:-translate-y-4 hover:scale-105 duration-700">
+            <OrganizationIcon color="primary-500" width="50" />
+            <h1 className="text-primary-50 text-lg 2xl:text-2xl font-bold">Student Council of SMTI Yogyakarta Vocational High School</h1>
+            <h1 className="text-primary-50 text-md 2xl:text-xl">Skills and Entrepreneurship (Danus)</h1>
+            <h1 className="text-primary-50 text-sm 2xl:text-lg">Sep 2023 - Present </h1>
+          </div>
+          <div className="flex flex-col gap-1 h-1/3 bg-gradient-to-br from-dark-2 to-dark border-dark border-2 shadow-lg rounded-xl p-10 hover:-translate-y-4 hover:scale-105 duration-700">
+            <JapanIcon color="primary-500" width="50" />
+            <h1 className="text-primary-50 text-lg 2xl:text-2xl font-bold">Language Japanese Club (AOTAKA CLUB)</h1>
+            <h1 className="text-primary-50 text-md 2xl:text-xl">General Treasurer</h1>
+            <h1 className="text-primary-50 text-sm 2xl:text-lg">2022 - June 2024</h1>
+          </div>
+          <div className="flex flex-col gap-1 h-1/3 bg-gradient-to-br from-dark-2 to-dark border-dark border-2 shadow-lg rounded-xl p-10 hover:-translate-y-4 hover:scale-105 duration-700">
+            <ElectionIcon color="primary-500" width="50" />
+            <h1 className="text-primary-50 text-lg 2xl:text-2xl font-bold">Student Council Election Commission (KPO)</h1>
+            <h1 className="text-primary-50 text-md 2xl:text-xl">Field Executive (Sie Pelaksana)</h1>
+            <h1 className="text-primary-50 text-sm 2xl:text-lg">Aug 2023 - Sep 2023</h1>
+          </div>
+        </div>
+      </div>
+      <RetroGrid />
+    </div>
+  )
+}
